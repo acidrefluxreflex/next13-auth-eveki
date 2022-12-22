89cfc7782befdb27e015b8afdca5c204200f18d7
kabuki <tanuki5sanuki3@icloud.com>
2022-12-22 14:55:59 +0900

    jiji

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,11 @@
 import Footer from "../components/Footer";
-import SplineApp from "../components/SplineAnimation";
+
 
 export default function About() {
   return (
     <div>
       <h1>About</h1>
-      <SplineApp />
+
       <Footer></Footer>
     </div>
   );
--- app/components/SplineAnimation.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import Spline from '@splinetool/react-spline';
-
-export default function SplineApp() {
-  return (
-    <Spline scene="https://prod.spline.design/YIRzI2YuGttA1Pvs/scene.splinecode" />
-  );
-}
