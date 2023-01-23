e826dfb6b18ea8d6730c260eb9590c6027577bb6
kabuki <tanuki5sanuki3@icloud.com>
2023-01-23 14:37:36 +0900

    gig

---
+++ app/jp/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Eveki｜個性が作れる会話AI</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="会話AI「Eveki」ならキャラクターや実在の人物をモデルにした人工知能を簡単に作ることができます。" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
